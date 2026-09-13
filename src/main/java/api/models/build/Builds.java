4e01bf36b5049269f1ec6f43607d913f1c501814
AlesiaSapezhka <Alesia_Sapezhka@epam.com>
2026-09-13 18:28:17 +0300

    Models API

---
+++ src/main/java/api/models/build/Builds.java
@@ -0,0 +1,19 @@
+package api.models.build;
+
+import api.models.BaseModel;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Builds extends BaseModel {
+    private Integer count;
+    private List<Build> build;
+}
